d03f1169b3eab295fc3873b2f31d432527c91c20
YumaMatsumura <yumapine@gmail.com>
2022-06-29 00:53:14 +0900

    fix main.cpp

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@
 #define ANSI_COLOR_BOLD_BLUE  "\033[1;34m"
 #define ANSI_COLOR_BOLD_RED   "\033[1;31m"
 #define ANSI_COLOR_BOLD_GREEN "\033[1;32m"
-#define ANSI_COLOR_BACK_BLUE  "\033[37;44m"
-#define ANSI_COLOR_BACK_RED   "\033[37;41m"
-#define ANSI_COLOR_BACK_GREEN "\033[37;42m"
+#define ANSI_COLOR_BG_BLUE  "\033[37;44m"
+#define ANSI_COLOR_BG_RED   "\033[37;41m"
+#define ANSI_COLOR_BG_GREEN "\033[37;42m"
 
 int main(int argc, char ** argv)
 {
@@ -21,9 +21,9 @@ int main(int argc, char ** argv)
   std::cout << ANSI_COLOR_BOLD_RED "This text is bold red." ANSI_COLOR_RESET << std::endl;
   std::cout << ANSI_COLOR_BOLD_GREEN "This text is bold green." ANSI_COLOR_RESET << std::endl;
 
-  std::cout << ANSI_COLOR_BACK_BLUE "This text is backgroud blue." ANSI_COLOR_RESET << std::endl;
-  std::cout << ANSI_COLOR_BACK_RED "This text is backgroud red." ANSI_COLOR_RESET << std::endl;
-  std::cout << ANSI_COLOR_BACK_GREEN "This text is backgroud green." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BG_BLUE "This text is backgroud blue." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BG_RED "This text is backgroud red." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BG_GREEN "This text is backgroud green." ANSI_COLOR_RESET << std::endl;
   
   return 0;
 }
