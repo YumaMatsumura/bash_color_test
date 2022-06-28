7b299281c6cb8593f8155157c85934c9bfedd039
YumaMatsumura <yumapine@gmail.com>
2022-06-29 00:37:09 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# bash_color_text
+## コンパイル
+```
+g++ -o bash_color_test main.cpp
+```
+
+## 実行
+```
+./bash_color_test
+```
+
+## 参考
+- https://qiita.com/ko1nksm/items/095bdb8f0eca6d327233
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#define ANSI_COLOR_RESET "\033[0m"
+#define ANSI_COLOR_BLUE  "\033[34m"
+#define ANSI_COLOR_RED   "\033[31m"
+#define ANSI_COLOR_GREEN "\033[32m"
+#define ANSI_COLOR_BOLD_BLUE  "\033[1;34m"
+#define ANSI_COLOR_BOLD_RED   "\033[1;31m"
+#define ANSI_COLOR_BOLD_GREEN "\033[1;32m"
+#define ANSI_COLOR_BACK_BLUE  "\033[37;44m"
+#define ANSI_COLOR_BACK_RED   "\033[37;41m"
+#define ANSI_COLOR_BACK_GREEN "\033[37;42m"
+
+int main(int argc, char ** argv)
+{
+  std::cout << ANSI_COLOR_BLUE "This text is blue." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_RED "This text is red." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_GREEN "This text is green." ANSI_COLOR_RESET << std::endl;
+
+  std::cout << ANSI_COLOR_BOLD_BLUE "This text is bold blue." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BOLD_RED "This text is bold red." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BOLD_GREEN "This text is bold green." ANSI_COLOR_RESET << std::endl;
+
+  std::cout << ANSI_COLOR_BACK_BLUE "This text is backgroud blue." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BACK_RED "This text is backgroud red." ANSI_COLOR_RESET << std::endl;
+  std::cout << ANSI_COLOR_BACK_GREEN "This text is backgroud green." ANSI_COLOR_RESET << std::endl;
+  
+  return 0;
+}
